d51036967c03a13c84774a2cb723e6e954b3656d
Nodirbek <friend18337@gmail.com>
2024-02-02 16:46:54 +0500

    Searching by title and sorting by id is complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,17 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { LazyLoadEvent } from 'primeng/api';
 import { of } from 'rxjs';
-import { Observable, pluck } from 'rxjs';
+import { Observable } from 'rxjs';
 import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  value: string = 'hello';
+
   difficultyColors: any = [
     'success',
     'success',
@@ -21,7 +23,8 @@ export class AppComponent {
     'danger',
   ];
 
-  private apiUrl = 'https://kep.uz/api/problems?';
+  titleFilter: string = '';
+  sortOrder: string = '';
 
   constructor(private http: HttpClient, private cdr: ChangeDetectorRef) {}
 
@@ -30,7 +33,7 @@ export class AppComponent {
   pageNumber = 1;
 
   ngOnInit(): void {
-    this.data$ = this.getDat(1, 81).pipe(pluck('data'));
+    this.loadProblems({ first: 0, rows: 10 });
   }
 
   loadProblems(event: LazyLoadEvent) {
@@ -38,22 +41,75 @@ export class AppComponent {
       ? (this.pageNumber = 1)
       : (this.pageNumber = (event.first ?? 81) / (event.rows ?? 81) + 1);
 
-    this.getDat(this.pageNumber, event.rows ?? 81).subscribe(
-      (response: any) => {
+    let ordering = '';
+
+    if (this.sortOrder === 'asc') {
+      ordering = 'id';
+    } else if (this.sortOrder === 'desc') {
+      ordering = '-id';
+    }
+
+    if (this.titleFilter) {
+      const lowercaseTitle = this.titleFilter.toLowerCase();
+
+      this.getData(
+        this.pageNumber,
+        event.rows ?? 81,
+        lowercaseTitle,
+        ordering
+      ).subscribe((response: any) => {
         this.totalData = response.total;
         this.data$ = of(response.data);
 
         this.cdr.detectChanges();
-      }
-    );
+      });
+    } else {
+      this.getData(
+        this.pageNumber,
+        event.rows ?? 81,
+        undefined,
+        ordering
+      ).subscribe((response: any) => {
+        this.totalData = response.total;
+        this.data$ = of(response.data);
+
+        this.cdr.detectChanges();
+      });
+    }
   }
 
-  getDat(page: number, page_size: number) {
-    return this.http.get('https://kep.uz/api/problems?', {
-      params: {
-        page: page.toString(),
-        page_size: page_size.toString(),
-      },
+  getData(page: number, page_size: number, title?: string, ordering?: string) {
+    let params: any = {
+      page: page.toString(),
+      page_size: page_size.toString(),
+    };
+
+    if (title) {
+      params.title = title;
+    }
+
+    if (ordering) {
+      params.ordering = ordering;
+    }
+
+    return this.http.get('https://kep.uz/api/problems', {
+      params: params,
     });
+  }
+
+  titleSearch(event: Event) {
+    const inputValue = (event.target as HTMLInputElement).value;
+    this.titleFilter = inputValue;
+    this.loadProblems({ first: 0, rows: 10 });
+  }
+
+  toggleSortOrder() {
+    // Toggle the sorting order when the button is clicked
+    this.sortOrder = this.sortOrder === 'asc' ? 'desc' : 'asc';
+    this.loadProblems({ first: 0, rows: 10 });
+  }
+
+  getRotationAngle(): string {
+    return this.sortOrder === 'asc' ? 'rotate(-90deg)' : 'rotate(90deg)';
   }
 }
